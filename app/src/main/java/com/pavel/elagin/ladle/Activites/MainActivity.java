4cfcf29e2c600d06807aba7ca39321f4cead060a
Pavel Elagin <elagin.pasha@gmail.com>
2016-11-29 10:49:02 +0300

    Добавлено EditRecActivity

--- app/src/main/java/com/pavel/elagin/ladle/Activites/MainActivity.java
+++ app/src/main/java/com/pavel/elagin/ladle/Activites/MainActivity.java
@@ -1,7 +1,9 @@
-package com.pavel.elagin.ladle;
+package com.pavel.elagin.ladle.Activites;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.pavel.elagin.ladle.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/pavel/elagin/ladle/MyApp.java
@@ -0,0 +1,8 @@
+package com.pavel.elagin.ladle;
+
+/**
+ * Created by pavel on 25.11.16.
+ */
+
+public class MyApp {
+}
