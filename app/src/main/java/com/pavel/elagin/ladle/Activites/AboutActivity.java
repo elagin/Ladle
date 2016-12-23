013f5eb9a03d10623772acf81571d72fba76caf9
Pavel Elagin <elagin.pasha@gmail.com>
2016-12-23 10:24:59 +0300

    Сова на логотипе. Добавлено "спасибо" в окно О приложении.

--- app/src/main/java/com/pavel/elagin/ladle/Activites/AboutActivity.java
+++ app/src/main/java/com/pavel/elagin/ladle/Activites/AboutActivity.java
@@ -18,5 +18,9 @@ public class AboutActivity extends AppCompatActivity {
         TextView about_feedback = (TextView) findViewById(R.id.about_feedback);
         about_feedback.setText(Html.fromHtml(getString(R.string.feedback)));
         about_feedback.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView about_logo_rights = (TextView) findViewById(R.id.about_thanks);
+        about_logo_rights.setText(Html.fromHtml(getString(R.string.about_thanks)));
+        about_logo_rights.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
