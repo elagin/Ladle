4d3d3db5d379c0fc1cfd46be9f7045fc8c44a513
Pavel Elagin <elagin.pasha@gmail.com>
2017-02-13 10:09:15 +0300

    Отображаем сообщение когда список покупок пустой.

--- app/src/main/java/ru/crew4dev/forksnknife/Activites/ShoppingActivity.java
+++ app/src/main/java/ru/crew4dev/forksnknife/Activites/ShoppingActivity.java
@@ -3,6 +3,7 @@ package ru.crew4dev.forksnknife.Activites;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -22,22 +23,25 @@ public class ShoppingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping);
 
-        table_shopping_list_view = (TableLayout) findViewById(R.id.table_shopping_list_view);
-
         List<Ingredient> shoppingList = MyApp.getShopingList();
-        if (shoppingList != null)
+        if (shoppingList != null) {
+            findViewById(R.id.textView_shopping_list_is_empty).setVisibility(View.GONE);
+            table_shopping_list_view = (TableLayout) findViewById(R.id.table_shopping_list_view);
             for (int i = 0; i < shoppingList.size(); i++) {
-                Ingredient item = shoppingList.get(i);
-                addIng(item.name, item.count, item.unit);
+                Ingredient Ingredient = shoppingList.get(i);
+                addIng(Ingredient);
             }
+        }
+        else
+            findViewById(R.id.textView_shopping_list_is_empty).setVisibility(View.VISIBLE);
     }
 
-    private void addIng(String name, Double count, String unit) {
+    private void addIng(Ingredient ingredient) {
         final int index = table_shopping_list_view.getChildCount();
         TableRow row = (TableRow) LayoutInflater.from(this).inflate(R.layout.shopping_item_row, null);
-        ((TextView) row.findViewById(R.id.ing_name)).setText(name);
-        ((TextView) row.findViewById(R.id.ing_count)).setText(count.toString().replace(".0", ""));
-        ((TextView) row.findViewById(R.id.ing_unit)).setText(unit);
+        ((TextView) row.findViewById(R.id.ing_name)).setText(ingredient.name);
+        ((TextView) row.findViewById(R.id.ing_count)).setText(ingredient.count.toString().replace(".0", ""));
+        ((TextView) row.findViewById(R.id.ing_unit)).setText(ingredient.unit);
         row.setId(index);
         table_shopping_list_view.addView(row);
     }
